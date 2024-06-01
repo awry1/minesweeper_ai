46127e34d46c4a5430dd9b8f4971ccce220064a4
Awry <92302998+awry1@users.noreply.github.com>
2024-06-02 00:35:40 +0200

    Update minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -1,4 +1,5 @@
 import random
+from sympy import *
 
 
 def create_boards(size, num_mines, seed):
@@ -19,29 +20,23 @@ def create_boards(size, num_mines, seed):
 
 def print_board(board):
     size = len(board)
-    print(' ', end = " ")
+    print(' ')
+    print('+' + '-' * (2 * size - 1) + '+')
+    for row in board:
+        print('|' + ' '.join(row) + '|')
+    print('+' + '-' * (2 * size - 1) + '+')
+
+
+def print_board_border(board):
+    size = len(board)
+    print('\n ', end=" ")
     for i in range(size):
-        print(str(i), end = " ")
+        print(str(i), end=" ")
     print(' ')
     print(' +' + '-' * (2 * size - 1) + '+')
-    row_number = 0
-    for row in board:
-        print(str(row_number) + '|' + ' '.join(row) + '|')
-        row_number += 1
+    for i, row in enumerate(board):
+        print(str(i) + '|' + ' '.join(row) + '|')
     print(' +' + '-' * (2 * size - 1) + '+')
-
-
-def ai_take_input(size, game_started, player_board):
-    if not game_started:
-        row, col = random.randint(0, size - 1), random.randint(0, size - 1)
-    else:
-        row, col = solve_analytical(size, player_board)
-        # if len(moves) == 0:
-        #   moves = solve_analytical(size, player_board)
-        # row, col = moves.pop
-
-    print('AI chose:', col, row, "\n")
-    return row, col
 
 
 def take_input():
@@ -98,14 +93,37 @@ def print_end_board(game_board, player_board):
             if game_board[row][col] == 'X':
                 player_board[row][col] = 'X'
     print_board(player_board)
+            
+
+def gameloop(size, num_mines, seed):
+    game_board, player_board = create_boards(size, num_mines, seed)
+    while True:
+        print_board_border(player_board)
+
+        row, col = take_input()
+        if not is_input_valid(row, col, size):
+            print('Invalid input!')
+            continue
+
+        if is_mine(game_board, row, col):
+            print_end_board(game_board, player_board)
+            print('You Lose!')
+            quit()
+
+        else:
+            reveal_squares(game_board, player_board, row, col)
+            if is_game_finished(game_board, player_board):
+                print_board(player_board)
+                print('You Win!')
+                quit()
 
 
-# AI part
+########################## AI part starts here ##########################
 def find_undiscovered_fields(size, player_board):
     undiscovered = []
     for row in range(size):
         for col in range(size):
-            if not player_board[row][col] == " ":       # or if [row, col] in mines
+            if not player_board[row][col] == " ":  # or if [row, col] in mines (will this be better?)
                 continue
             neighbours = find_adjacent_numbers([row, col], size, player_board)
             if len(neighbours) > 0:
@@ -127,12 +145,7 @@ def find_adjacent_numbers(position, size, player_board):
 
 
 def solve_gauss(matrix):
-    # debug print matrix
-    print("\nMatrix:")
-    for row in matrix:
-        print(' '.join(['+' + str(element) if element != 0 else ' _' for element in row]))
-    
-    n = len(matrix)
+    n = len(matrix)     # Number of rows in the matrix
     m = len(matrix[0])  # Number of columns in the matrix
 
     for i in range(min(n, m - 1)):  # Ensure we do not exceed the number of columns
@@ -172,22 +185,46 @@ def solve_gauss(matrix):
                     for l in range(m):
                         matrix[k][l] -= factor * matrix[i][l]
                 break
-        
-    # debug print solved matrix and quit
-    print("\nSolved matrix:")
-    for row in matrix:
-        print(' '.join(['+' + str(element) if element > 0 else str(element) if element < 0 else ' _' for element in row]))
-    
-    quit()
 
     return matrix
 
 
+def find_moves_and_mines(solved_matrix, undiscovered):
+    moves = []
+    mines = []
+
+    n = len(solved_matrix)
+    m = len(solved_matrix[0])
+
+    # Represent undiscovered fields as symbols from x0 to x(m-1)
+    hidden_fields = symbols(" ".join(f"x{x}" for x in range(m - 1)), real=True)
+
+    # Create the coefficient matrix (A) and the result vector (b)
+    A = Matrix([[solved_matrix[i][j] for j in range(m - 1)] for i in range(n)])
+    b = Matrix([solved_matrix[i][m - 1] for i in range(n)])
+
+    # Solve the system of equations
+    solution = linsolve((A, b), *hidden_fields)
+
+    i = -1
+    for sol in solution:
+        for element in sol:
+            i += 1
+            if element == 1:
+                mines.append(undiscovered[i][0])
+            elif element == 0:
+                moves.append(undiscovered[i][0])
+            else:
+                continue
+
+    return moves, mines
+
+
 def solve_analytical(size, player_board):
-    undiscovered = find_undiscovered_fields(size, player_board)     # every undiscoverd field with adjacent numbers
-    unique_numbers = []     # list of unique adjacent numbers
+    undiscovered = find_undiscovered_fields(size, player_board)  # every undiscoverd field with adjacent numbers
+    unique_numbers = []  # list of unique adjacent numbers
     for field in undiscovered:
-        for number_pos in field[1]:     # undiscovered.neighbours
+        for number_pos in field[1]:  # undiscovered.neighbours
             if number_pos not in unique_numbers:
                 unique_numbers.append(number_pos)
 
@@ -205,52 +242,88 @@ def solve_analytical(size, player_board):
     # overwrite last column with values instead of coordinates
     for i in range(len(matrix)):
         number_pos = matrix[i][len(undiscovered)]
-        matrix[i][len(undiscovered)] = ord(player_board[number_pos[0]][number_pos[1]]) - 48    # convert number from board to int
+        matrix[i][len(undiscovered)] = ord(
+            player_board[number_pos[0]][number_pos[1]]) - 48  # convert number from board to int
 
     # solve with Gauss elimination
-    solve_gauss(matrix)
+    solved_matrix = solve_gauss(matrix)
 
     # find moves and mines
-    # append moves
-    # append mines
+    moves, mines = find_moves_and_mines(solved_matrix, undiscovered)
 
-    # return moves
+    return moves, mines
 
-    row = None
-    col = None
+
+def ai_take_input(size, game_started, player_board):
+    row, col = None, None
+
+    if not game_started:
+        row, col = random.randint(0, size - 1), random.randint(0, size - 1)
+
+    else:
+        moves, mines = solve_analytical(size, player_board)
+        if len(moves) > 0:
+            row = moves[0][0]
+            col = moves[0][1]
+
+    print('\nAI chose:', col, row)
     return row, col
+
+
+def ai_gameloop(size, num_mines, seed):
+    game_board, player_board = create_boards(size, num_mines, seed)
+    game_started = False
+    while True:
+        print_board(player_board)
+
+        row, col = ai_take_input(size, game_started, player_board)
+        if row is None or col is None:
+            print('\nUndecided')
+            return '?'
+
+        if is_mine(game_board, row, col):
+            print_end_board(game_board, player_board)
+            if not game_started:
+                print('\nLose on first')
+                return 'L1'
+            print('\nLose')
+            return 'L'
+
+        else:
+            game_started = True
+            reveal_squares(game_board, player_board, row, col)
+            if is_game_finished(game_board, player_board):
+                print_board(player_board)
+                print('\nWin')
+                return 'W'
+            
+
+def simulation(size, num_mines, seed, iterations=100):
+        wins, loses, loses1, undecided = 0, 0, 0, 0
+
+        for _ in range(iterations):
+            result = ai_gameloop(size, num_mines, seed)
+            if result == '?':
+                undecided += 1
+            elif result == 'W':
+                wins += 1
+            elif result == 'L1':
+                loses1 += 1
+            else:
+                loses += 1
+
+        print(f"\nWins: {wins}, Loses: {loses}, Loses on first: {loses1}, Undecided: {undecided}")
+        quit()
 
 
 if __name__ == '__main__':
     size = 10
     num_mines = 10
-    seed = 10
-    game_started = False
+    seed = None
 
     AI = True
 
-    game_board, player_board = create_boards(size, num_mines, seed)
-    while True:
-        print_board(player_board)
-
-        if AI:
-            row, col = ai_take_input(size, game_started, player_board)
-            if not game_started:
-                game_started = True
-        else:
-            row, col = take_input()
-            if not is_input_valid(row, col, size):
-                print('Invalid input!')
-                continue
-
-        if is_mine(game_board, row, col):
-            print_end_board(game_board, player_board)
-            print('You lose!')
-            break
-
-        else:
-            reveal_squares(game_board, player_board, row, col)
-            if is_game_finished(game_board, player_board):
-                print_end_board(game_board, player_board)
-                print('You win!')
-                break
+    if AI:
+        simulation(size, num_mines, seed)
+    else:
+        gameloop(size, num_mines, seed)
